61a2246438b7b8ce71b6601a04d3ae26f194d6f8
Emmanuel Raju <eraj007@gmail.com>
2025-04-17 01:41:45 +0530

    chore: integrates supabase

--- src/routes/wip/+page.svelte
+++ src/routes/wip/+page.svelte
@@ -4,6 +4,22 @@
 	import { hidePageLoader } from '$lib/utils/common';
 	import { onMount } from 'svelte';
 
+	import { supabase } from '$lib/utils/supabaseClient';
+	import { PUBLIC_SUPABASE_STORAGE_BUCKET } from '$env/static/public';
+
+	let audioUrl = '';
+
+	const loadAudio = async () => {
+		const { data } = supabase.storage
+			.from(PUBLIC_SUPABASE_STORAGE_BUCKET)
+			.getPublicUrl('music/00-Opening-Prayer.mp3');
+
+		audioUrl = data.publicUrl;
+		console.log('Audio URL:', audioUrl);
+	};
+
+	loadAudio();
+
 	onMount(async () => {
 		const pageLoaded = await hidePageLoader();
 	});
@@ -12,4 +28,5 @@
 <main>
 	<Hero title="Emmanuel"></Hero>
 	<!-- <StackedCards></StackedCards> -->
+	<audio controls src={audioUrl}></audio>
 </main>
---
+++ src/routes/wip/music/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import { PUBLIC_SUPABASE_STORAGE_BUCKET } from '$env/static/public';
+	import { hidePageLoader } from '$lib/utils/common';
+	import { supabase } from '$lib/utils/supabaseClient';
+	import { onMount } from 'svelte';
+
+	let musicFiles: string[] = [];
+
+	const loadMusicFiles = async () => {
+		const { data, error } = await supabase.storage
+			.from(PUBLIC_SUPABASE_STORAGE_BUCKET)
+			.list('music', {
+				limit: 10,
+				offset: 0,
+				sortBy: { column: 'name', order: 'asc' }
+			});
+
+		console.log('Music files:', data);
+
+		if (error) {
+			console.error('Error loading music files:', error.message);
+			return;
+		}
+
+		musicFiles = data.map(
+			(item) =>
+				supabase.storage.from(PUBLIC_SUPABASE_STORAGE_BUCKET).getPublicUrl(`music/${item.name}`)
+					.data.publicUrl
+		);
+	};
+
+	loadMusicFiles();
+
+	onMount(async () => {
+		const pageLoaded = await hidePageLoader();
+	});
+</script>
+
+<h1 class="mb-4 text-2xl font-bold">Sermons</h1>
+
+{#if musicFiles.length > 0}
+	<ul class="space-y-4">
+		{#each musicFiles as url}
+			<li class="rounded-lg border p-4 shadow">
+				<audio controls src={url} class="w-full"></audio>
+			</li>
+		{/each}
+	</ul>
+{:else}
+	<p>No music files found.</p>
+{/if}
